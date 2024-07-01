4af89bdd1dc594e4020be2da4fdb0097fec52270
Dimas Fajar <153111942+fajardison@users.noreply.github.com>
2024-07-01 22:43:50 +0700

    Create clearCache.mjs

---
+++ cache/clearCache.mjs
@@ -0,0 +1,31 @@
+/**
+ * @license MIT
+ * Hak Cipta (c) 2024 Dimas Fajar
+ * 
+ * Menghapus semua entri dari cache.
+ * 
+ * Fungsi ini terlebih dahulu mengautentikasi permintaan menggunakan token yang diberikan.
+ * Jika berhasil diautentikasi, fungsi ini akan menghapus semua entri dalam cache dengan mereset objek cache.
+ * 
+ * @param {string} token - Token autentikasi.
+ * @returns {Promise<void>} Sebuah promise yang terselesaikan ketika cache berhasil dihapus.
+ * @throws {Error} Menghasilkan error jika autentikasi gagal.
+ * 
+ * @example
+ * // Contoh penggunaan:
+ * const token = 'my-auth-token';
+ * try {
+ *   await clearCache(token);
+ *   console.log('Cache berhasil dihapus.');
+ * } catch (error) {
+ *   console.error('Error menghapus cache:', error.message);
+ * }
+ */
+export default async function clearCache(token) {
+  if (this.authenticate(token)) {
+    this.cache = {};
+    return Promise.resolve();
+  } else {
+    return Promise.reject(new Error('Unauthorized'));
+  }
+}
