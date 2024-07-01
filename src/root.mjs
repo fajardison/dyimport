6fdb5bfcda5d3324b70b8f4fea2fa9cbffbce52d
Dimas Fajar <153111942+fajardison@users.noreply.github.com>
2024-07-01 22:12:45 +0700

    Create root.mjs

---
+++ src/root.mjs
@@ -0,0 +1,34 @@
+/**
+ * @license MIT
+ * (c) 2024 Dimas Fajar
+ * @description Modul ini menyediakan fungsi untuk membaca data dari file `package.json`.
+ * Fungsi ini mencari field "root" di dalam `package.json` dan mengembalikan nilainya.
+ */
+
+'use strict';
+
+import fs from 'fs/promises';
+import path from 'path';
+
+/**
+ * Membaca data dari file package.json.
+ * @returns {Promise<Object>} - Data yang ditemukan di dalam field "root" dari package.json.
+ * @throws {Error} - Jika terjadi kesalahan dalam membaca atau parsing file package.json, atau jika field "root" tidak ditemukan.
+ * @description Fungsi ini membaca file package.json dan mengembalikan data dari field "root".
+ */
+export async function loadData() {
+  try {
+    const filePath = path.resolve('package.json');
+    const data = await fs.readFile(filePath, 'utf-8');
+    const jsonData = JSON.parse(data);
+    
+    if (jsonData.root) {
+      return jsonData.root; // Mengembalikan field "root"
+    } else {
+      throw new Error('Field "root" not found in package.json');
+    }
+  } catch (error) {
+    console.error('Error reading package.json:', error);
+    throw error; // Menyebarkan error ke caller
+  }
+}
