148883d7cb346af750dbb471a2d947c5de3437ff
Dimas Fajar <153111942+fajardison@users.noreply.github.com>
2024-07-01 22:11:47 +0700

    Create initUtils.mjs

---
+++ src/initUtils.mjs
@@ -0,0 +1,24 @@
+/**
+ * @description Modul ini menyediakan fungsi untuk inisialisasi Secret dan Cache.
+ * @license MIT
+ * (c) 2024 Dimas Fajar
+ */
+
+'use strict';
+
+import Secret from '../keys/secret.mjs';
+import Cache from '../cache/cache.mjs';
+
+/**
+ * Inisialisasi Secret dan Cache.
+ * @returns {{authToken: string, accessToken: string, cache: Cache}} - Objek yang berisi authToken, accessToken, dan cache.
+ */
+export function initialize() {
+  const mySecret = new Secret('sha3-512');
+  mySecret.generateRandomData(512);
+  const hash = mySecret.createHash();
+  const authToken = hash;
+  const accessToken = hash;
+  const cache = new Cache(authToken, 100);
+  return { authToken, accessToken, cache };
+}
