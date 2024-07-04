c6e6cc2ac733d32c982ee6fcb9aa4bf1a8124555
Dimas Fajar <153111942+fajardison@users.noreply.github.com>
2024-07-04 11:33:00 +0700

    Create testmodule.js

---
+++ test/testmodule.js
@@ -0,0 +1,13 @@
+/**
+ * @module testFile
+ * @description File ini hanya untuk keperluan pengujian. Berisi fungsi atau objek yang akan diimpor oleh loadModule.
+ */
+
+// Fungsi sederhana yang akan diimpor oleh loadModule
+export function test1() {
+  return 'this is a test using dyimport.';
+}
+
+export function test2() {
+  return 'test successful.';
+}
